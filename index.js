8d8c21631dc8bf0894ea5b6856df8cf8015b3919
namantyagi971 <naman.tyagi971@gmail.com>
2021-03-13 11:56:03 +0530

    setup express server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-hello
+// require the express library
+const express = require('express');
+
+// fire up the express server
+const app = express();
+
+// port to run the server
+const port = 8000;
+
+// checking whether server is running or not
+app.listen(port,function(err){
+    if(err)
+    {
+        console.log(`Error in running the server :  ${err}`);
+        return;
+    }
+    console.log(`Server is up and running on port : ${port}`);
+});
